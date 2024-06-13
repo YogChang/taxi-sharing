63c2d82e0254394b9f1bf1f7b76842ccc20e002e
Wen-Yao Chang <yog.chang@gosaico.com>
2024-06-13 15:08:38 +0800

    add post

--- app.py
+++ app.py
@@ -1,15 +1,35 @@
-from flask import Flask
-import os,ctypes,json,codecs,subprocess
+from flask import Flask, render_template, request
+import os
+import ctypes
+import json
 import urllib.parse
+import time
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    with codecs.open('./test_data/input/parameter.json','r','utf8') as f:
-        input_data = json.load(f,)
+@app.route('/')
+def hello_world():
+    return render_template('search.html')
+
+@app.route('/cautare', methods=['POST'])
+def get_val():
+    val = request.form.get("Titlul_cartii")
+    if val is None:
+        return "No data received", 400
+    return authors_recommendations(val)
+
+def authors_recommendations(input_data):
+    start_time = time.time()
+
+    # Mock result for testing purpose
     result_data = RunPyDispatch(input_data)
     
+    # Simulate long computation time (remove this in production)
+    time.sleep(5)  # Remove this line if you have an actual long computation
+    
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+
     # 将 data 转换为 JSON 字符串
     data_json = json.dumps(result_data['geo_json'])
     
@@ -18,27 +38,29 @@ def hello():
     
     # 创建超链接
     ret = f"<a href='http://geojson.io/#data=data:application/json,{data_encoded}'> show result on http://geojson.io </a>"
+    ret += f"<br>Calculation time: {elapsed_time:.2f} seconds"
     
     return ret
 
-
-def RunPyDispatch(input_data,THIS_SO_PATH="./lib/",AB_SO_PATH="/usr/local/lib"):
-
-    ortools_lib = ctypes.cdll.LoadLibrary(os.path.join(AB_SO_PATH,"libortools.so.9")) # libortools 通常在 usr/local/lib
-    project_lib = ctypes.cdll.LoadLibrary(os.path.join(THIS_SO_PATH,"libsharing_manager.so"))        # 自己專案編譯的 .so
-
+def RunPyDispatch(input_data, THIS_SO_PATH="./lib/", AB_SO_PATH="/usr/local/lib"):
+    ortools_lib = ctypes.cdll.LoadLibrary(os.path.join(AB_SO_PATH, "libortools.so.9")) # libortools 通常在 usr/local/lib
+    project_lib = ctypes.cdll.LoadLibrary(os.path.join(THIS_SO_PATH, "libsharing_manager.so")) # 自己專案編譯的 .so
 
     # ctypes 型別 const char*(const char*)
     project_lib.PyDispatch.argtypes = [ctypes.c_char_p]
     project_lib.PyDispatch.restype = ctypes.c_char_p
     print("start Dispatch ...")
     
-    bytes_data = json.dumps(input_data).encode("utf-8")
+    # 确保 input_data 为字符串
+    if isinstance(input_data, str):
+        bytes_data = input_data.encode("utf-8")
+    else:
+        bytes_data = json.dumps(input_data).encode("utf-8")
 
     # PyDispatch = C++ 函式名:  json_obj --> string --> json_bytes ---> C++ ---> bytes --> string ----> json_obj
-    return json.loads(project_lib.PyDispatch(bytes_data).decode("utf-8"))
-
+    result = project_lib.PyDispatch(bytes_data)
+    decoded_result = result.decode("utf-8")
+    return json.loads(decoded_result)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
-
