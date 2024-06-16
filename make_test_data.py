0cfb7bb06f47340994b3787e34a38327ef9fb414
YogChang <yogchang@DESKTOP-COTIM4T>
2024-06-16 10:27:29 +0800

    edit_make_test_data

--- make_test_data.py
+++ make_test_data.py
@@ -19,42 +19,39 @@ def vehicles():
     off_work_time_range = [1020, 1320]
     capacity_range = [3, 6]
 
-    ret = []
-    for _ in range(vehicles_size):
-        ret.append({
-            "code": 'V-{:03}'.format(_),
+    return [
+        {
+            "code": 'V-{:03}'.format(index),
             "on_work_time": random.randint(on_work_time_range[0], on_work_time_range[1]),
             "off_work_time": random.randint(off_work_time_range[0], off_work_time_range[1]),
             "capacity":  random.randint(capacity_range[0], capacity_range[1])
-        })
-    
-    return ret
+        } for index in range(vehicles_size)
+    ]
 
 
-
-bali = [25.149837552461616, 121.40405342515518]
-lake = [24.939932971435937, 121.63988127252611]
-
-orders_size = 80
-start_time_range = [360, 1020]
-duration_time_range = [40, 100]
-headcount_range = [1, 3]
-
-
-ave_latitude = (bali[0] + lake[0]) / 2
-ave_longitude = (lake[1] + bali[1]) / 2
-sigma_lat = (bali[0] - lake[0]) / 4
-sigma_long = (lake[1] - bali[1]) / 4
 
 def orders():
+    orders_size = 80
+    bali = [25.149837552461616, 121.40405342515518]
+    lake = [24.939932971435937, 121.63988127252611]
 
-    ret = []
-    for _ in range(orders_size):
-        start_time = random.randint(start_time_range[0], start_time_range[1])
-        ret.append({
-            "code":  'O-{:04}'.format(_),
-            "start_time": start_time,
-            "end_time": start_time + random.randint(duration_time_range[0], duration_time_range[1]),
+    start_time_range = [360, 1020]
+    duration_time_range = [40, 100]
+    headcount_range = [1, 3]
+
+
+    ave_latitude = (bali[0] + lake[0]) / 2
+    ave_longitude = (lake[1] + bali[1]) / 2
+    sigma_lat = (bali[0] - lake[0]) / 4
+    sigma_long = (lake[1] - bali[1]) / 4
+
+    start_time_list = [random.randint(start_time_range[0], start_time_range[1]) for index in range(orders_size)]
+
+    return [
+        {
+            "code":  'O-{:04}'.format(i),
+            "start_time": val,
+            "end_time": val + random.randint(duration_time_range[0], duration_time_range[1]),
             "headcount": random.randint(headcount_range[0], headcount_range[1]),
             "direct_location": {
                 "latitude": random.lognormvariate(0, sigma_lat) + ave_latitude -1,
@@ -64,21 +61,20 @@ def orders():
                 "latitude": random.lognormvariate(0, sigma_lat) + ave_latitude -1,
                 "longitude": random.lognormvariate(0, sigma_long) + ave_longitude -1
             }
-        })
+        } for i, val in enumerate(start_time_list)
+    ]
 
-    return ret
 
 
 def strategy():
-    ret = {
+    return {
         "first_solution_strategy": 8,
         "metaheuristic": 3,
         "time_limit": 30
     }
-    return ret
 
-def main():
 
+if __name__ == '__main__':
     ret = {
         "vehicles": vehicles(),
         "orders": orders(),
@@ -87,8 +83,3 @@ def main():
 
     SaveJson('./test_data/input/parameter.json',ret)
     print("make test data done")
-
-
-
-if __name__ == '__main__':
-    main()
